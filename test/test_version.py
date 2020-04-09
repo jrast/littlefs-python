2bb4b54df47f5f9069279376382a8595d5dae25f
Jürg Rast <juergr@gmail.com>
2020-04-09 23:56:01 +0200

    Updated littlefs to 2.2.1

--- test/test_version.py
+++ test/test_version.py
@@ -2,6 +2,6 @@ import littlefs
 
 def test_version():
     """Test if the versions of littlefs can be imported"""
-    assert littlefs.__LFS_VERSION__ == (2, 1)
+    assert littlefs.__LFS_VERSION__ == (2, 2)
     assert littlefs.__LFS_DISK_VERSION__ == (2, 0)
     
