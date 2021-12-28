38473388c133fbe6c081b68d8f598acc0f33f973
Cesar Vandevelde <cesar.vandevelde@gmail.com>
2021-12-28 23:32:22 +0100

    Use io module classes for FileHandle and open()
    These changes make littlefs file objects behave similar to standard Python
    file-like objects:
    
    - Files can be opened in either text or binary mode, and will return str or
      bytes respectively.
    - read() will now read until EOF unless length is specified.
    - open() now supports all standard mode flags, e.g. 'a' for append, 'w+' for
      read/write, etc.
    - open() raises standard exceptions (e.g. FileNotFoundError) where applicable.

--- src/littlefs/__init__.py
+++ src/littlefs/__init__.py
@@ -1,9 +1,13 @@
-from typing import TYPE_CHECKING, List, Tuple, Optional, Iterator
+import io
+import warnings
+from typing import TYPE_CHECKING, List, Tuple, Iterator, IO
 
 from pkg_resources import DistributionNotFound, get_distribution
 
 from . import errors, lfs
 from .lfs import __LFS_DISK_VERSION__, __LFS_VERSION__
+from .errors import LittleFSError
+
 
 try:
     __version__ = get_distribution('littlefs-python').version
@@ -43,14 +47,134 @@ class LittleFS:
         """Mount the underlying buffer"""
         return lfs.mount(self.fs, self.cfg)
 
-    def open(self, fname: str, mode='r') -> 'FileHandle':
+    def open(
+        self,
+        fname: str,
+        mode='r',
+        buffering: int = -1,
+        encoding: str = None,
+        errors: str = None,
+        newline: str = None
+    ) -> IO:
         """Open a file"""
-        fh = lfs.file_open(self.fs, fname, mode)
-        return FileHandle(self.fs, fh)
+
+        # Parse mode
+        creating = False
+        reading = False
+        writing = False
+        appending = False
+        updating = False
+
+        binary = False
+        text = False
+
+        for ch in mode:
+            if ch == "x":
+                creating = True
+            elif ch == "r":
+                reading = True
+            elif ch == "w":
+                writing = True
+            elif ch == "a":
+                appending = True
+            elif ch == "+":
+                updating = True
+            elif ch == "b":
+                binary = True
+            elif ch == "t":
+                text = True
+            else:
+                raise ValueError(f"invalid mode: '{mode}'")
+
+        if text and binary:
+            raise ValueError("can't have text and binary mode at once")
+
+        exclusive_modes = (creating, reading, writing, appending)
+
+        if sum(int(m) for m in exclusive_modes) > 1:
+            raise ValueError(
+                "must have exactly one of create/read/write/append mode"
+            )
+
+        if binary:
+            if encoding is not None:
+                raise ValueError(
+                    "binary mode doesn't take an encoding argument"
+                )
+
+            if errors is not None:
+                raise ValueError(
+                    "binary mode doesn't take an errors argument"
+                )
+
+            if newline is not None:
+                raise ValueError(
+                    "binary mode doesn't take a newline argument"
+                )
+
+            if buffering == 1:
+                msg = (
+                    "line buffering (buffering=1) isn't supported in "
+                    "binary mode, the default buffer size will be used"
+                )
+                warnings.warn(msg, RuntimeWarning)
+                buffering = -1
+
+        try:
+            fh = lfs.file_open(self.fs, fname, mode)
+        except LittleFSError as e:
+            # Try to map to standard Python exceptions
+            if e.code == LittleFSError.Error.LFS_ERR_NOENT:
+                raise FileNotFoundError from e
+            elif e.code == LittleFSError.Error.LFS_ERR_ISDIR:
+                raise IsADirectoryError from e
+            elif e.code == LittleFSError.Error.LFS_ERR_EXIST:
+                raise FileExistsError from e
+            else:
+                raise e
+
+        raw = FileHandle(self.fs, fh)
+
+        line_buffering = False
+
+        if buffering == 1:
+            buffering = -1
+            line_buffering = True
+
+        if buffering < 0:
+            buffering = self.cfg.cache_size
+
+        if buffering == 0:
+            if not binary:
+                raise ValueError("can't have unbuffered text I/O")
+
+            return raw
+
+        if updating:
+            buffered = io.BufferedRandom(raw, buffering)
+        elif creating or writing or appending:
+            buffered = io.BufferedWriter(raw, buffering)
+        elif reading:
+            buffered = io.BufferedReader(raw, buffering)
+        else:
+            raise ValueError(f"Unknown mode: '{mode}'")
+
+        if binary:
+            return buffered
+
+        wrapped = io.TextIOWrapper(
+            buffered,
+            encoding,
+            errors,
+            newline,
+            line_buffering
+        )
+
+        return wrapped
 
     def listdir(self, path='.') -> List[str]:
         """List directory content
-        
+
         List the content of a directory. This function uses :meth:`scandir`
         internally. Using :meth:`scandir` might be better if you are
         searching for a specific file or need access to the :class:`littlefs.lfs.LFSStat`
@@ -161,7 +285,7 @@ class LittleFS:
         """Generate the file names in a directory tree
 
         Generate the file and directory names in a directory tree by
-        walking the tree top-down. This functions closely resembels the 
+        walking the tree top-down. This functions closely resembels the
         behaviour of :func:`os.stat`.
 
         Each iteration yields a tuple containing three elements:
@@ -179,31 +303,84 @@ class LittleFS:
                 files.append(elem.name)
             elif elem.type == 2:
                 dirs.append(elem.name)
-        
+
         yield top, dirs, files
         for dirname in dirs:
             newtop = '/'.join((top, dirname)).replace('//', '/')
             yield from self.walk(newtop)
 
 
-
-class FileHandle:
-
+class FileHandle(io.RawIOBase):
     def __init__(self, fs, fh):
+        super().__init__()
+
         self.fs = fs
         self.fh = fh
 
-    def __enter__(self):
-        return self
+    def close(self):
+        # Base implementation is not used to avoid extra call to flush().
+        # LittleFS already flushes the file on close.
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
+        if not self.closed:
+            lfs.file_close(self.fs, self.fh)
+            setattr(self, "__IOBase_closed", True)
+
+    def readable(self):
+        return lfs.LFSFileFlag.rdonly in self.fh.flags and not self.closed
+
+    def writable(self):
+        return lfs.LFSFileFlag.wronly in self.fh.flags and not self.closed
+
+    def seekable(self):
+        self._checkClosed()
+        return True
+
+    def seek(self, offset, whence=io.SEEK_SET):
+        # Whence constants are reused from the io module. The constants have
+        # the same as LFS_SEEK_SET / LFS_SEEK_CUR / LFS_SEEK_END.
+        self._checkClosed()
+        return lfs.file_seek(self.fs, self.fh, offset, whence)
+
+    def tell(self):
+        self._checkClosed()
+        return lfs.file_tell(self.fs, self.fh)
+
+    def truncate(self, size=None) -> int:
+        self._checkClosed()
+
+        pos = self.tell()
+        ret = lfs.file_truncate(self.fs, pos)
+
+        return ret
 
     def write(self, data):
-        return lfs.file_write(self.fs, self.fh, data)
+        self._checkClosed()
+        self._checkWritable()
 
-    def read(self, size):
+        return lfs.file_write(self.fs, self.fh, bytes(data))
+
+    def readinto(self, b):
+        self._checkClosed()
+        self._checkReadable()
+
+        req_len = len(b)
+        result = lfs.file_read(self.fs, self.fh, req_len)
+        res_len = len(result)
+
+        b[0:res_len] = result
+
+        return res_len
+
+    def readall(self):
+        self._checkClosed()
+        self._checkReadable()
+
+        file_size = lfs.file_size(self.fs, self.fh)
+        file_pos = self.tell()
+        size = file_size - file_pos
+
         return lfs.file_read(self.fs, self.fh, size)
 
-    def close(self):
-        lfs.file_close(self.fs, self.fh)
+    def flush(self):
+        super().flush()
+        lfs.file_sync(self.fs, self.fh)
