7c9b8d63ec7ff06c4fda823c43c4136f3f174651
Ladislav Láska <krakonos@krakonos.org>
2023-08-04 20:39:47 +0200

    Bumped littlefs to version 2.7.

--- test/test_version.py
+++ test/test_version.py
@@ -2,5 +2,5 @@ import littlefs
 
 def test_version():
     """Test if the versions of littlefs can be imported"""
-    assert littlefs.__LFS_VERSION__ == (2, 6)
+    assert littlefs.__LFS_VERSION__ == (2, 7)
     assert littlefs.__LFS_DISK_VERSION__ == (2, 1)
