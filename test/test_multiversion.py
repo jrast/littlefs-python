283d71bb25b88df7d49404ea40c6b60a37eefca2
Jürg Rast <jrast@hamilton.ch>
2023-08-07 10:56:33 +0200

    More tests for multiversion support

--- test/test_multiversion.py
+++ test/test_multiversion.py
@@ -1,5 +1,15 @@
 import pytest
-from littlefs import LittleFS, FileHandle
+from littlefs import LittleFS, FileHandle, LittleFSError
+
+
+def create_image(version):
+    fs = LittleFS(block_size=128, block_count=64, disk_version=version)
+    fs.mkdir('testdir')
+    with fs.open('file.txt', 'w') as f:
+        f.write('Sample Content')
+    
+    return fs.context.buffer
+
 
 @pytest.fixture(scope='function')
 def fs20():
@@ -26,3 +36,20 @@ def test_lfs20(fs20):
 
 def test_lfs21(fs21):
     assert fs21.fs_stat().disk_version == 0x00020001
+
+
+@pytest.mark.parametrize('version', [0x00020000, 0x00020001], ids=['FS2.0', 'FS2.1'])
+def test_load_images(version):
+    fs = LittleFS(block_size=128, block_count=64, disk_version=0x00020001)
+    fs.context.buffer = create_image(version)
+    fs.mount()
+    stat = fs.fs_stat()
+    assert stat.disk_version == version
+
+
+
+def test_load_incompatible_image():
+    fs = LittleFS(block_size=128, block_count=64, disk_version=0x00020000)
+    fs.context.buffer = create_image(0x00020001)
+    with pytest.raises(LittleFSError, match='-22: LFS_ERR_INVAL'):
+        assert fs.mount()    
