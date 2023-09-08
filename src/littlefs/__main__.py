c93963a773b5a19f393d78e15a1b05992fc8ab35
J. Rast <juergr@gmail.com>
2023-09-08 19:07:39 +0200

    Improved code

--- src/littlefs/__main__.py
+++ src/littlefs/__main__.py
@@ -5,17 +5,8 @@ import pathlib
 from littlefs import LittleFS
 
 
-def _add_default_args(parser: argparse.ArgumentParser):
-    parser.add_argument("-v", "--verbose", action="count", default=0)
-
-
-def _add_littlefs_args(parser: argparse.ArgumentParser):
-    parser.add_argument("-s", "--block-size", type=int, help="LittleFS block size")
-    parser.add_argument("-c", "--block-count", type=int, help="LittleFS block count")
-    parser.add_argument("-i", "--image", help="LittleFS filesystem image", type=pathlib.Path)
-
-
-def _create(args: argparse.Namespace):
+def create(args: argparse.Namespace):
+    """Create LittleFS image from directory content"""
     if args.block_size is None or args.block_count is None:
         print("Block size and block count is required to create an image.")
         return -1
@@ -30,20 +21,20 @@ def _create(args: argparse.Namespace):
         else:
             print("Adding File:     ", rel_path)
             with fs.open(rel_path.as_posix(), "wb") as dest:
-                with open(path, "rb") as src:
-                    dest.write(src.read())
-    with open(args.image, "wb") as fp:
-        fp.write(fs.context.buffer)
+                dest.write(path.read_bytes())
+
+    args.image.write_bytes(fs.context.buffer)
+    return 0
 
 
 def _list(args: argparse.Namespace):
+    """List LittleFS image content"""
     if args.block_size is None or args.block_count is None:
         print("Block size and block count is required to create an image.")
         return -1
 
     fs = LittleFS(block_size=args.block_size, block_count=args.block_count)
-    with open(args.image, "rb") as fp:
-        fs.context.buffer = bytearray(fp.read())
+    fs.context.buffer = bytearray(args.image.read_bytes())
 
     fs.mount()
     for root, dirs, files in fs.walk("/"):
@@ -53,9 +44,11 @@ def _list(args: argparse.Namespace):
             print(f"{root}{dir}")
         for file in files:
             print(f"{root}{file}")
+    return 0
 
 
-def _unpack(args: argparse.Namespace):
+def unpack(args: argparse.Namespace):
+    """Unpack LittleFS image to directory"""
     if args.block_size is None or args.block_count is None:
         print("Block size and block count is required to create an image.")
         return -1
@@ -68,6 +61,7 @@ def _unpack(args: argparse.Namespace):
     root_dest = root_dest.absolute()
     if not root_dest.exists() or not root_dest.is_dir():
         print("Destination directory does not exist")
+        return -1
 
     fs.mount()
     for root, dirs, files in fs.walk("/"):
@@ -92,7 +86,7 @@ def main():
     if sys.argv[0].endswith("__main__.py"):
         prog = f"python -m littlefs"
     else:
-        prog = f"python-littlefs"
+        prog = f"littlefs-python"
 
     parser = argparse.ArgumentParser(
         prog=prog,
@@ -106,28 +100,31 @@ def main():
         # formatter_class=argparse.RawTextHelpFormatter,
     )
 
+    common_parser = argparse.ArgumentParser(add_help=False)
+    common_parser.add_argument("-v", "--verbose", action="count", default=0)
+    common_parser.add_argument("-s", "--block-size", type=int, help="LittleFS block size")
+    common_parser.add_argument("-c", "--block-count", type=int, help="LittleFS block count")
+    common_parser.add_argument("-i", "--image", help="LittleFS filesystem image", type=pathlib.Path)
+
     subparsers = parser.add_subparsers(required=True, title="available commands", dest="command")
 
-    parser_create = subparsers.add_parser(
-        "create", help="Create LittleFS image from directory content"
-    )
+    def add_command(handler, name="", help=""):
+        subparser = subparsers.add_parser(
+            name or handler.__name__, parents=[common_parser], help=help or handler.__doc__
+        )
+        subparser.set_defaults(func=handler)
+        return subparser
+
+    parser_create = add_command(create)
     parser_create.add_argument("source", help="Source Directory", type=pathlib.Path)
 
-    parser_unpack = subparsers.add_parser("unpack", help="Unpack LittleFS image to directory")
+    parser_unpack = add_command(unpack)
     parser_unpack.add_argument("destination", help="Destination Directory", type=pathlib.Path)
 
-    parser_list = subparsers.add_parser("list", help="List LittleFS image content")
-
-    for subparser in subparsers.choices.values():
-        _add_default_args(subparser)
-        _add_littlefs_args(subparser)
+    parser_list = add_command(_list, "list")
 
     args = parser.parse_args(sys.argv[1:])
-
-    print("Verbosity:", args.verbose)
-
-    meth = {"create": _create, "unpack": _unpack, "list": _list}[args.command]
-    return meth(args)
+    return args.func(args)
 
 
 if __name__ == "__main__":
