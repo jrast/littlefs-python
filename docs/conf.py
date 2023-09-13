407f1688f1f570f1a7436d79ef6b3ac1f87cba17
J. Rast <juergr@gmail.com>
2023-09-13 22:28:39 +0200

    Added readthedocs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -7,17 +7,18 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'littlefs-python'
-copyright = '2019, Jürg Rast'
-author = 'Jürg Rast'
+project = "littlefs-python"
+copyright = "2019 - 2023, Jürg Rast"
+author = "Jürg Rast"
 
 # The full version, including alpha/beta/rc tags
 from pkg_resources import get_distribution
-version = get_distribution('littlefs-python').version
+
+version = get_distribution("littlefs-python").version
 release = version
 
 # The master doc must be set for read the docs.
-master_doc = 'index'
+master_doc = "index"
 
 
 # -- General configuration ---------------------------------------------------
@@ -26,19 +27,19 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -46,17 +47,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Napoleon settings
 napoleon_numpy_docstring = True
 
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None)
-}
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
