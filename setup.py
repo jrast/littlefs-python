546c8ea93c7fecdeb41b074c256cc45885b2d9c4
Brian Pugh <bnp117@gmail.com>
2023-12-01 21:20:22 -0800

    update python 3.12 identifier and readme. CI is already building 3.12 wheels.

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering",
         "Topic :: Software Development :: Embedded Systems",
         "Topic :: Software Development :: Libraries",
